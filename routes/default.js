3ff88b24a73b807eb9d9af6ecf55faa9d8cdff23
Yuk Fang <yfa@macbook-pro.local>
2020-12-25 17:43:58 +0800

    add echo params

--- routes/default.js
+++ routes/default.js
@@ -4,9 +4,13 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.writeHead(200, {'content-type': 'application/json'});
-  let msg = '';
-  msg += JSON.stringify(req.headers, null, 2);
-  res.write(msg);
+  let headers =  JSON.stringify(req.headers, null, 2);
+  let params =   JSON.stringify(req.query, null, 2);
+
+  res.write('headers:');
+  res.write(headers);
+  res.write(' params:');
+  res.write(params);
   res.end();
 });
 
