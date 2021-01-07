97c56ef8b53f5c22e014ff493a6239b897a34153
fangyu.k <fangyu.k@bytedance.com>
2021-01-07 18:56:26 +0800

    add body

--- routes/default.js
+++ routes/default.js
@@ -20,11 +20,16 @@ router.get('/', function(req, res, next) {
   let path    =  JSON.stringify(req.path, null, 2);
   let headers =  JSON.stringify(req.headers, null, 2);
   let params =   JSON.stringify(req.query, null, 2);
+  let body =   JSON.stringify(req.body, null, 2);
 
+  res.write(' path:');
   res.write(path);
+  res.write(' headers:');
   res.write(headers);
   res.write(' params:');
   res.write(params);
+  res.write(' body:');
+  res.write(body);
   res.end();
 });
 
