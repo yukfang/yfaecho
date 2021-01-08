f3466514163947ce339ec77859f6fca185846641
fangyu.k <fangyu.k@bytedance.com>
2021-01-08 11:29:01 +0800

    add body for POST

--- routes/default.js
+++ routes/default.js
@@ -3,9 +3,10 @@ var router = express.Router();
 
 function handlereq(req, res, next) {
   res.writeHead(200, {'content-type': 'application/json'});
+  let path    =  JSON.stringify(req.path, null, 2);
   let headers =  JSON.stringify(req.headers, null, 2);
   let params =   JSON.stringify(req.query, null, 2);
-
+  let body =   JSON.stringify(req.body, null, 2);
 
   res.write(' path:');
   res.write(path);
