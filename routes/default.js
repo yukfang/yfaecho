2f25dd18d5680983d31858e7e4ffbf8bb9c1a5d9
fangyu.k <fangyu.k@bytedance.com>
2021-01-08 11:43:35 +0800

    debug

--- routes/default.js
+++ routes/default.js
@@ -16,28 +16,11 @@ function handlereq(req, res, next) {
     'body' : body
   };
 
-  res.write(JSON.stringify(resp));
+  res.write(JSON.stringify(resp, null, 2));
   res.end();
 }
 
 router.post('/', handlereq);
-
-router.get('/', function(req, res, next) {
-  res.writeHead(200, {'content-type': 'application/json'});
-  let path    =  JSON.stringify(req.path, null, 2);
-  let headers =  JSON.stringify(req.headers, null, 2);
-  let params =   JSON.stringify(req.query, null, 2);
-  let body =   JSON.stringify(req.body, null, 2);
-
-  res.write(' path:');
-  res.write(path);
-  res.write(' headers:');
-  res.write(headers);
-  res.write(' params:');
-  res.write(params);
-  res.write(' body:');
-  res.write(body);
-  res.end();
-});
+router.get('/', handlereq);
 
 module.exports = router;
