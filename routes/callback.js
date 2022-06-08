0e503494149671b3386f8b09d8b2899dbafd2873
方宇 <fangyu.k@bytedance.com>
2022-06-08 22:04:07 +0800

    text

--- routes/callback.js
+++ routes/callback.js
@@ -3,42 +3,42 @@ const Request = require('request');
 var fs = require('fs');
 
 const { head } = require('../app');
+const app = require('../app');
 var router = express.Router();
 
 function handleCb(req, res, next) {
-  res.writeHead(200, {'content-type': 'application/json'});
-  let path    =  req.baseUrl;
-  let params  =  req.query;
-  let body    =  req.body;
-  let headers =  req.headers;
+  if(req.query.indexOf('cbmonitor') == -1) {
+    res.writeHead(200, {'content-type': 'application/json'});
+    let path    =  req.baseUrl;
+    let params  =  req.query;
+    let body    =  req.body;
+    let headers =  req.headers;
 
-  let resp = {
-     "dir": __dirname,
-     body,
-     path,
-     headers,
-     params
-  };
+    let resp = {
+      "dir": __dirname,
+      body,
+      path,
+      headers,
+      params
+    };
 
-  let output = JSON.stringify(resp, null, 2);
+    let output = JSON.stringify(resp, null, 2);
+    app.locals.cbdata = resp;
 
-  fs.writeFile(__dirname + '/../cbdata.txt', output, function (err) {
-    if (err) throw err;
-    console.log('Saved!');
-  });
-
-  curlFileUpload();
-
-  res.write({code:0});
-  res.end();
+    res.write({code:0});
+    res.end();
+  } else {
+    res.writeHead(200, {'content-type': 'application/json'});
+    res.write(app.locals.cbdata);
+    res.end();
+  }
 }
 
 
 function curlFileUpload(callback) {
-
   var curlCommand;
   curlCommand = 'curl -X PUT "https://yfadisk.scm.azurewebsites.net/api/vfs/site/wwwroot/bf/" --header "If-Match: *"  --header "Authorization: Basic ' +
-    process.env.APPSETTING_FILE_UPLOAD_KEY + '" --upload-file "../cbdata.txt"';
+    process.env.APPSETTING_FILE_UPLOAD_KEY + '" --upload-file "cbdata.txt"';
   var exec = require('child_process').exec;
   var child = exec(curlCommand);
   var contents = '';
