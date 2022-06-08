fa2c439aa559693ffced357ab5aa6a65f640121a
方宇 <fangyu.k@bytedance.com>
2022-06-08 21:14:58 +0800

    add push

--- routes/callback.js
+++ routes/callback.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const Request = require('request');
 var fs = require('fs');
 
 const { head } = require('../app');
@@ -21,14 +22,42 @@ function handleCb(req, res, next) {
 
   let output = JSON.stringify(resp, null, 2);
 
-  fs.writeFile(__dirname + 'mynewfile3.txt', output, function (err) {
+  fs.writeFile(__dirname + '/../cbdata.txt', output, function (err) {
     if (err) throw err;
     console.log('Saved!');
   });
 
-  res.write(output);
+  curlFileUpload();
+
+  res.write({code:0});
   res.end();
 }
+
+
+function curlFileUpload(callback) {
+
+  var curlCommand;
+  curlCommand = 'curl -X PUT "https://yfadisk.scm.azurewebsites.net/api/vfs/site/wwwroot/bf/" --header "If-Match: *"  --header "Authorization: Basic ' +
+    process.env.APPSETTING_FILE_UPLOAD_KEY + '" --upload-file "../cbdata.txt"';
+  var exec = require('child_process').exec;
+  var child = exec(curlCommand);
+  var contents = '';
+  child.stdout.on('data', function(data) {
+      contents += data;
+      console.log(contents);
+  });
+  child.stderr.on('data', function(data) {
+      //console.log('error: ' + data);
+  });
+  child.on('close', function(code) {
+      try {
+          contents = JSON.parse(contents);
+      } catch(e) { }
+      if ( callback ) {
+          callback(contents);
+      }
+  });
+};
 
 router.post('/', handleCb);
 router.get('/', handleCb);
